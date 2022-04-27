68178c4fc67f23dc5c9d8d2bc9fe03d2f4cf9a76
Michał Mieszczak <michal@mieszczak.com.pl>
2022-04-27 02:25:10 +0200

    Add git and nvim to testvm. Fix bootdrive

--- hosts/testvm/default.nix
+++ hosts/testvm/default.nix
@@ -8,12 +8,15 @@
 
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.device = "/dev/sda";
 
   networking.hostName = "testvm";
 
   networking.useDHCP = false;
   networking.interfaces.enp1s0.useDHCP = true;
+
+  programs.neovim = { enable = true; defaultEditor = true; };
+  programs.git = { enable = true; };
 
   services.openssh.enable = true;
   system.stateVersion = "21.11";
