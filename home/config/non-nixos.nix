3d79edee6644467e10c9dda4edb4f7bb88c72600
Michał Mieszczak <michal@mieszczak.com.pl>
2022-05-02 13:16:58 +0200

    Add experimental nix features to non-nixos configuration

--- home/config/non-nixos.nix
+++ home/config/non-nixos.nix
@@ -4,6 +4,15 @@
   home.packages = with pkgs; [
     hostname
     unstable.home-manager
+    # This must be here, enable option below does not ensure that nix is installed
     unstable.nix
   ];
+
+  nix = {
+    enable = true;
+    package = pkgs.unstable.nix;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
 }
