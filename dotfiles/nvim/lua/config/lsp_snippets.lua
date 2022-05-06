a5304f82a3209f0a10a88726610d7e13c849c48d
Michał Mieszczak <michal@mieszczak.com.pl>
2022-05-06 14:46:00 +0200

    Fix nvim snippets (for some reason I used compe template instead of cmp)

--- dotfiles/nvim/lua/config/lsp_snippets.lua
+++ dotfiles/nvim/lua/config/lsp_snippets.lua
@@ -3,13 +3,12 @@ local remap = vim.api.nvim_set_keymap
 -- Snippets
 local function prequire(...)
 	local status, lib = pcall(require, ...)
-	if status then
-		return lib
-	end
+	if (status) then return lib end
 	return nil
 end
 
-local luasnip = prequire("luasnip")
+local luasnip = prequire('luasnip')
+local cmp = prequire("cmp")
 
 local t = function(str)
 	return vim.api.nvim_replace_termcodes(str, true, true, true)
@@ -25,24 +24,26 @@ local check_back_space = function()
 end
 
 _G.tab_complete = function()
-	if vim.fn.pumvisible() == 1 then
-		return t("<C-n>")
+	if cmp and cmp.visible() then
+		cmp.select_next_item()
 	elseif luasnip and luasnip.expand_or_jumpable() then
 		return t("<Plug>luasnip-expand-or-jump")
 	elseif check_back_space() then
 		return t("<Tab>")
 	else
-		return vim.fn["compe#complete"]()
+		cmp.complete()
 	end
+	return ""
 end
 _G.s_tab_complete = function()
-	if vim.fn.pumvisible() == 1 then
-		return t("<C-p>")
+	if cmp and cmp.visible() then
+		cmp.select_prev_item()
 	elseif luasnip and luasnip.jumpable(-1) then
 		return t("<Plug>luasnip-jump-prev")
 	else
 		return t("<S-Tab>")
 	end
+	return ""
 end
 
 -- Remaps
