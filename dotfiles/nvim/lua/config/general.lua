d6eeb990d222f092d5875602164f570cba726758
Michał Mieszczak <michal@mieszczak.com.pl>
2022-06-06 08:36:48 +0200

    [Neovim] Set markdown indentation

--- dotfiles/nvim/lua/config/general.lua
+++ dotfiles/nvim/lua/config/general.lua
@@ -29,6 +29,7 @@ local indent_map = {
 	cpp = { tabstop = 2, shiftwidth = 2, expandtab = true },
 	nix = { tabstop = 2, shiftwidth = 2, expandtab = true },
 	js = { tabstop = 4, shiftwidth = 4, expandtab = true },
+	markdown = { tabstop = 4, shiftwidth = 4, expandtab = true },
 }
 local group = vim.api.nvim_create_augroup("MyCustomIndents", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
