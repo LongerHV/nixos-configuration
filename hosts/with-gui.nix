b5056e4054a6b7fc81c677f2e29f6295eca9ce04
Michał Mieszczak <michal@mieszczak.com.pl>
2022-06-08 17:27:32 +0200

    [Mordor] Repalce plasma with gnome

--- hosts/with-gui.nix
+++ hosts/with-gui.nix
@@ -10,10 +10,11 @@
   };
 
   services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
   services.xserver.layout = "pl";
 
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   sound.enable = true;
   services.pipewire = {
