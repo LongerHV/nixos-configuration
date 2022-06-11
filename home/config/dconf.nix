64248e9e082d4ccb867d08679dba9bf2473cee7f
Michał Mieszczak <michal@mieszczak.com.pl>
2022-06-11 20:02:09 +0200

    [Mordor] Use fetchurl to download wallpaper and set it in gnome

--- home/config/dconf.nix
+++ home/config/dconf.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }:
 
+let
+  nierWallpaper = builtins.fetchurl {
+    url = "https://images6.alphacoders.com/655/655990.jpg";
+    sha256 = "b09b411a9c7fc7dc5be312ca9e4e4b8ee354358daa792381f207c9f4946d95fe";
+  };
+in
 {
   dconf.settings = {
     "org/gnome/desktop/peripherals/trackball" = {
@@ -10,5 +16,12 @@
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
     };
+    "org/gnome/desktop/background" = {
+      picture-uri = "file://${nierWallpaper}";
+      picture-uri-dark = "file://${nierWallpaper}";
+      };
+    "org/gnome/desktop/screensaver" = {
+      picture-uri = "file://${nierWallpaper}";
+      };
   };
 }
