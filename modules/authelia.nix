8aeb4c5e19e340c9e2c4fcacf140377563469b92
Michał Mieszczak <michal@mieszczak.com.pl>
2022-06-16 18:46:36 +0200

    [Nasgul] Move authelia module to native package

--- modules/authelia.nix
+++ modules/authelia.nix
@@ -6,52 +6,68 @@ let
   cfg = config.services.authelia;
   opt = options.services.authelia;
   rootDir = "/var/lib/authelia";
-  configFile = builtins.toJSON cfg.settings;
-  configFilePath = "/etc/authelia/configuration.yml";
+  configFile = writeTextFile {
+    name = "configuration.yml";
+    text = builtins.toJSON cfg.settings;
+  };
 in
 {
   options.services.authelia = {
     enable = mkEnableOption "authelia";
-    jwtSecretFile = mkOption {
-      type = types.path;
-    };
     image = mkOption {
       type = types.str;
       default = "authelia/authelia:4.35.6";
     };
-    storageEncryptionKeyFile = mkOption {
+    dataDir = mkOption {
+      default = "/var/lib/authelia";
       type = types.path;
     };
-    port = mkOption {
-      type = types.int;
-      default = 9092;
+    jwtSecretFile = mkOption {
+      type = types.path;
+    };
+    storageEncryptionKeyFile = mkOption {
+      type = types.path;
     };
     settings = mkOption {
       type = types.attrs;
       default = { };
     };
+    user = mkOption {
+      default = "authelia";
+      type = types.str;
+    };
+    group = mkOption {
+      default = "authelia";
+      type = types.str;
+    };
   };
 
   config = mkIf cfg.enable {
-    virtualisation.oci-containers.containers = {
-      authelia = rec {
-        inherit (cfg) image;
-        cmd = [ "--config" "/etc/authelia/configuration.yml" ];
-        ports = [ "${builtins.toString cfg.port}:9092" ];
-        environment = {
-          AUTHELIA_JWT_SECRET_FILE = "/run/jwt_secret";
-          AUTHELIA_STORAGE_ENCRYPTION_KEY_FILE = "/run/storage_encryption_file";
-        };
-        volumes = [
-          "${rootDir}:/config"
-          "${configFilePath}:${configFilePath}"
-          "${cfg.jwtSecretFile}:${environment.AUTHELIA_JWT_SECRET_FILE}"
-          "${cfg.storageEncryptionKeyFile}:${environment.AUTHELIA_STORAGE_ENCRYPTION_KEY_FILE}"
-        ];
+    systemd.tmpfiles.rules = [ "d '${cfg.dataDir}' 0700 authelia authelia - -" ];
+    systemd.services.authelia = {
+      description = "Authelia SSO service";
+      after = [ "network-online.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.authelia}/bin/authelia --config ${configFile}";
+        Type = "simple";
+        User = cfg.user;
+        Group = cfg.group;
+        Restart = "on-failure";
+      };
+      environment = {
+        AUTHELIA_JWT_SECRET_FILE = cfg.jwtSecretFile;
+        AUTHELIA_STORAGE_ENCRYPTION_KEY_FILE = cfg.storageEncryptionKeyFile;
       };
     };
-    environment.etc."authelia/configuration.yml" = {
-      text = configFile;
+
+    users.users."${cfg.user}" = {
+      inherit (cfg) group;
+      home = cfg.dataDir;
+      createHome = true;
+      isSystemUser = true;
     };
+
+    users.groups."${cfg.group}" = { };
   };
 }
