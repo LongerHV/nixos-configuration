adb4fc82586ba06d80ab63d2fc8f27811ee009a8
Michał Mieszczak <michal@mieszczak.com.pl>
2022-06-21 22:51:41 +0200

    [Nasgul] Enable prometheus and grafana

--- hosts/nasgul/selfhosted/monitoring.nix
+++ hosts/nasgul/selfhosted/monitoring.nix
@@ -6,13 +6,27 @@ in
 {
   services.traefik.dynamicConfigOptions.http = {
     routers.netdata_router = util.traefik_router { subdomain = "netdata"; };
+    routers.prometheus_router = util.traefik_router { subdomain = "prometheus"; };
+    routers.grafana_router = util.traefik_router { subdomain = "grafana"; };
     services.netdata_service = util.traefik_service { port = 19999; };
+    services.prometheus_service = util.traefik_service { port = 9090; };
+    services.grafana_service = util.traefik_service { port = 3001; };
   };
 
-  services.netdata = {
-    enable = true;
-    config.global = {
-      "update every" = "15";
+  services = {
+    netdata = {
+      enable = true;
+      config.global = {
+        "update every" = "15";
+      };
+    };
+    prometheus = {
+      enable = true;
+    };
+    grafana = {
+      enable = true;
+      port = 3001;
+      domain = "grafana.${config.myDomain}";
     };
   };
 }
