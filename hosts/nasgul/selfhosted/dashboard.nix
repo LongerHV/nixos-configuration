5cad4e08928c2f9cce8f9e2e16e8ff7f6e6216b6
Michał Mieszczak <michal@mieszczak.com.pl>
2022-06-21 22:51:58 +0200

    [Nasgul] Update dashboard

--- hosts/nasgul/selfhosted/dashboard.nix
+++ hosts/nasgul/selfhosted/dashboard.nix
@@ -5,28 +5,78 @@ let
     pageInfo = {
       title = "Longer's homelab";
       description = "Nyaa~~";
-      faviconApi = "local";
       navLinks = [
         {
           title = "GitHub";
-          path = "https://github.com/Lissy93/dashy";
+          path = "https://github.com/LongerHV";
         }
         {
-          title = "Documentation";
-          path = "https://dashy.to/docs";
+          title = "GitLab";
+          path = "https://gitlab.com/LongerHV";
         }
+
       ];
     };
     appConfig = {
       theme = "nord-frost";
       layout = "auto";
-      iconSize = "medium";
+      iconSize = "large";
       language = "pl";
+      faviconApi = "local";
+      hideComponents.hideSettings = true;
     };
     sections = [
       {
+        name = "Utilities";
+        items = [
+          {
+            title = "Dashy";
+            url = "https://dash.local.${config.myDomain}";
+            icon = "favicon";
+            statusCheck = true;
+          }
+          rec {
+            title = "Nextcloud";
+            url = "https://nextcloud.local.${config.myDomain}";
+            icon = "${url}/core/img/favicon.ico";
+            statusCheck = true;
+          }
+          {
+            title = "Gitea";
+            url = "https://gitea.local.${config.myDomain}";
+            icon = "favicon";
+            statusCheck = true;
+          }
+          rec {
+            title = "Traefik";
+            url = "https://traefik.local.${config.myDomain}";
+            icon = "${url}/dashboard/statics/icons/favicon.ico";
+            statusCheck = true;
+          }
+          rec {
+            title = "Authelia";
+            url = "https://auth.local.${config.myDomain}";
+            icon = "favicon";
+            statusCheck = true;
+          }
+          rec {
+            title = "Nix cache";
+            url = "https://cache.local.${config.myDomain}";
+            statusCheckUrl = "${url}/nix-cache-info";
+            icon = "https://raw.githubusercontent.com/NixOS/nixos-artwork/master/logo/nix-snowflake.svg";
+            statusCheck = true;
+          }
+        ];
+      }
+      {
         name = "Multimedia";
         items = [
+          rec {
+            title = "Jellyfin";
+            url = "https://jellyfin.local.${config.myDomain}";
+            icon = "${url}/web/favicon.ico";
+            statusCheck = true;
+          }
           {
             title = "Sonarr";
             url = "https://sonarr.local.${config.myDomain}";
@@ -39,22 +89,45 @@ let
             icon = "favicon";
             statusCheck = true;
           }
+          rec {
+            title = "Bazarr";
+            url = "https://bazarr.local.${config.myDomain}";
+            icon = "${url}/static/favicon.ico";
+            statusCheck = true;
+          }
           {
             title = "Prowlarr";
             url = "https://prowlarr.local.${config.myDomain}";
             icon = "favicon";
             statusCheck = true;
           }
+          rec {
+            title = "Transmission";
+            url = "https://transmission.local.${config.myDomain}";
+            icon = "${url}/transmission/web/images/favicon.ico";
+            statusCheck = true;
+          }
+        ];
+      }
+      {
+        name = "Monitoring";
+        items = [
           {
-            title = "Jellyfin";
-            url = "https://jellyfin.local.${config.myDomain}";
+            title = "Netdata";
+            url = "https://netdata.local.${config.myDomain}";
             icon = "favicon";
             statusCheck = true;
           }
           {
-            title = "Transmission";
-            url = "https://transmission.local.${config.myDomain}";
+            title = "Prometheus";
+            url = "https://prometheus.local.${config.myDomain}";
             icon = "favicon";
+            statusCheck = true;
+          }
+          rec {
+            title = "Grafana";
+            url = "https://grafana.local.${config.myDomain}";
+            icon = "${url}/public/img/grafana_icon.svg";
             statusCheck = true;
           }
         ];
