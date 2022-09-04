f29ff1fdc5abc38b05afbc688c62ac0de29dd992
Michał Mieszczak <michal@mieszczak.com.pl>
2022-09-04 12:40:19 +0200

    [Nasgul] Add placeholder for loki

--- hosts/nasgul/selfhosted/monitoring.nix
+++ hosts/nasgul/selfhosted/monitoring.nix
@@ -7,9 +7,11 @@ in
   services.traefik.dynamicConfigOptions.http = {
     routers.netdata_router = util.traefik_router { subdomain = "netdata"; middlewares = [ "authelia" ]; };
     routers.prometheus_router = util.traefik_router { subdomain = "prometheus"; middlewares = [ "authelia" ]; };
+    # routers.loki_router = util.traefik_router { subdomain = "loki"; middlewares = [ "authelia" ]; };
     routers.grafana_router = util.traefik_router { subdomain = "grafana"; middlewares = [ "authelia" ]; };
     services.netdata_service = util.traefik_service { port = 19999; };
     services.prometheus_service = util.traefik_service { port = 9090; };
+    # services.loki_service = util.traefik_service { port = 8083; };
     services.grafana_service = util.traefik_service { port = 3001; };
   };
 
@@ -23,6 +25,13 @@ in
     prometheus = {
       enable = true;
     };
+    # loki = {
+    #   enable = true;
+    #   configuration = {
+    #     auth_enabled = false;
+    #     server.http_listen_port = 8083;
+    #   };
+    # };
     grafana = {
       enable = true;
       port = 3001;
