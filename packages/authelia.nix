cadfeff20ca3c9895f5af3e712d3b1bd0d42b8a6
Michał Mieszczak <michal@mieszczak.com.pl>
2022-09-12 17:47:27 +0200

    [Nasgul] Update authelia

--- packages/authelia.nix
+++ packages/authelia.nix
@@ -2,7 +2,7 @@
 
 let
   pname = "authelia";
-  version = "4.36.3";
+  version = "4.36.7";
 in
 stdenv.mkDerivation {
   inherit pname;
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
 
   src = fetchzip {
     url = "https://github.com/${pname}/${pname}/releases/download/v${version}/${pname}-v${version}-linux-amd64.tar.gz";
-    sha256 = "sha256-dNNqm+m5uOC5V5Qx537eSQa6IhmlNoSHNPS1GOrmwN0=";
+    sha256 = "sha256-tEOAFdzJAl8d0clFoxxl5iBKf1RNlihN4MvM/E55rb4=";
     stripRoot = false;
   };
 
