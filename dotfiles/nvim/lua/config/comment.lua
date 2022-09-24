84aa5bd26316717b82f29a7856a60313578b1a48
Michał Mieszczak <michal@mieszczak.com.pl>
2022-09-24 17:35:18 +0200

    [Neovim] Fix comment bindings

--- dotfiles/nvim/lua/config/comment.lua
+++ dotfiles/nvim/lua/config/comment.lua
@@ -1,8 +1,17 @@
-require("Comment").setup()
-vim.api.nvim_set_keymap("n", "", '<cmd>lua require("Comment.api").toggle_current_linewise()<cr>', {})
+require("Comment").setup({
+	toggler = {
+		line = "",
+	},
+	mappings = {
+		basic = true,
+		extra = false,
+		extended = false,
+	}
+})
+
 vim.api.nvim_set_keymap(
 	"x",
 	"",
-	'<esc><cmd>lua require("Comment.api").toggle_blockwise_op(vim.fn.visualmode())<cr>',
+	'<esc><cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<cr>',
 	{}
 )
