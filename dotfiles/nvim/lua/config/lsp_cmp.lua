68c50a385ed28e6aa8834c394b9422b46ef650f0
Michał Mieszczak <michal@mieszczak.com.pl>
2022-10-17 18:06:20 +0200

    [Neovim] Update deprecated configs

--- dotfiles/nvim/lua/config/lsp_cmp.lua
+++ dotfiles/nvim/lua/config/lsp_cmp.lua
@@ -1,10 +1,5 @@
 vim.o.completeopt = "menuone,noselect,preview"
 
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 -- nvim-cmp setup
 local luasnip = require("luasnip")
 local cmp = require("cmp")
