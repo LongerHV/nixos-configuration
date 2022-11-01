ecdd15eef148e134e18152439541b3c7954b4f77
Michał Mieszczak <michal@mieszczak.com.pl>
2022-11-01 12:31:10 +0100

    [Nasgul] Update lldap

--- nixos/nasgul/selfhosted/lldap.nix
+++ nixos/nasgul/selfhosted/lldap.nix
@@ -4,7 +4,7 @@ let
   homeDir = "/var/lib/lldap";
   uid = 981;
   gid = 977;
-  tag = "v0.4";
+  tag = "v0.4.1";
 in
 {
   users = {
@@ -24,10 +24,9 @@ in
     image = "nitnelave/lldap:${tag}";
     environment = {
       TZ = "${config.time.timeZone}";
-      # UID = builtins.toString uid;
-      # GID = builtins.toString gid;
+      UID = builtins.toString uid;
+      GID = builtins.toString gid;
     };
-    user = "${builtins.toString uid}:${builtins.toString gid}";
     volumes = [
       "${homeDir}:/data"
     ];
