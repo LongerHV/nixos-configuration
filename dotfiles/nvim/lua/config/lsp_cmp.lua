bbff21ce60661afba7981320bceafec690125b61
Michał Mieszczak <michal@mieszczak.com.pl>
2022-11-11 20:12:14 +0100

    [Neovim] Update luasnip config

--- dotfiles/nvim/lua/config/lsp_cmp.lua
+++ dotfiles/nvim/lua/config/lsp_cmp.lua
@@ -1,5 +1,10 @@
 vim.o.completeopt = "menuone,noselect,preview"
 
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 -- nvim-cmp setup
 local luasnip = require("luasnip")
 local cmp = require("cmp")
@@ -27,21 +32,25 @@ cmp.setup({
 			select = true,
 		}),
 		["<Tab>"] = cmp.mapping(function(fallback)
-			if luasnip.expand_or_jumpable() then
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
 			else
 				fallback()
 			end
 		end, { "i", "s" }),
-
 		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if luasnip.jumpable(-1) then
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
 				luasnip.jump(-1)
 			else
 				fallback()
 			end
 		end, { "i", "s" }),
-
 	}),
 	sources = {
 		{ name = "nvim_lua" },
