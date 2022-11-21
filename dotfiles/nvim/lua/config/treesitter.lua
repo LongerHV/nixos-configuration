62f7fac9bdb47861fa8df739b725eb2d600c9a4d
Michał Mieszczak <michal@mieszczak.com.pl>
2022-11-21 13:00:08 +0100

    [Neovim] Update comments, disable treesitter auto install

--- dotfiles/nvim/lua/config/treesitter.lua
+++ dotfiles/nvim/lua/config/treesitter.lua
@@ -1,10 +1,12 @@
 require("nvim-treesitter.configs").setup({
-	-- ensure_installed = "all",
-	indent = { enable = true, disable = { "python", "yaml" } },
-	-- yati = { enable = true },
+	auto_install = false, -- Parsers are managed by Nix
+	indent = {
+		enable = true,
+		disable = { "python", "yaml" } -- Yaml and Python indents are unusable
+	},
 	highlight = {
 		enable = true,
-		disable = { "yaml" },
+		disable = { "yaml" }, -- Disable yaml highlighting because Helm sucks :<
 		additional_vim_regex_highlighting = false,
 	},
 	autopairs = { enable = true },
