78788d259ff289e155b7086874a6e3aa98b59e97
Michał Mieszczak <michal@mieszczak.com.pl>
2022-11-24 12:04:23 +0100

    [Home] Add default username and user directory as module

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,3 +1,9 @@
 {
+  default = { lib, ... }: {
+    home = rec {
+      username = lib.mkDefault "mmieszczak";
+      homeDirectory = lib.mkDefault "/home/${username}";
+    };
+  };
   nonNixos = import ./non-nixos.nix;
 }
