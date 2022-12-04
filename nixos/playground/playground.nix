4809b2856587fff766a00807764ee5cbd8f076ff
Michał Mieszczak <michal@mieszczak.com.pl>
2022-12-04 22:12:37 +0100

    [Playground] Cleanup

--- nixos/playground/playground.nix
+++ nixos/playground/playground.nix
@@ -1,19 +1,5 @@
 { config, pkgs, lib, inputs, outputs, ... }:
 
 {
-  homelab = {
-    domain = "playground.lan";
-    storage = "/data";
-    redis.enable = true;
-    mysql.enable = true;
-    mysql.package = pkgs.mariadb_106;
-    gitea.enable = true;
-    traefik.enable = true;
-    traefik.services = {
-      traefik = {
-        port = 8080;
-      };
-    };
-    multimedia.enable = true;
-  };
+
 }
