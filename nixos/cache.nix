3bd99358f4840b1ecb8a413ceaa83efa4ae0f30d
Michał Mieszczak <michal@mieszczak.com.pl>
2022-12-18 23:03:02 +0100

    [Mordor] Enable Nasgul Nix cache (disabled by accident)

--- nixos/cache.nix
+++ nixos/cache.nix
@@ -3,11 +3,11 @@ _:
 {
   nix.settings = {
     substituters = [
-      # "https://cache.local.longerhv.xyz"
+      "https://cache.local.longerhv.xyz"
       "https://cache.nixos.org"
     ];
     trusted-public-keys = [
-      # "cache.local.longerhv.xyz:ioE/YEOpla3uyof/kZQG+gNKgeBAhOMWh+riRAEzKDA="
+      "cache.local.longerhv.xyz:ioE/YEOpla3uyof/kZQG+gNKgeBAhOMWh+riRAEzKDA="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
