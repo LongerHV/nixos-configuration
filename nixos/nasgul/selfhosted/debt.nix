01f8a4e3de82bc8fa9c9657413f7f5f6774afd40
Michał Mieszczak <michal@mieszczak.com.pl>
2022-12-18 23:35:15 +0100

    [Nasgul] Remove redundant proxy configuration

--- nixos/nasgul/selfhosted/debt.nix
+++ nixos/nasgul/selfhosted/debt.nix
@@ -5,8 +5,4 @@ let
   util = pkgs.callPackage ./util.nix { inherit config; };
 in
 {
-  services.traefik.dynamicConfigOptions.http = {
-    routers.gitea_router = util.traefik_router { subdomain = "gitea"; };
-    services.gitea_service = util.traefik_service { port = 3000; };
-  };
 }
