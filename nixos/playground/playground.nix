c8c62fa7c90142810afd2053624a63dac978bb4d
Michał Mieszczak <michal@mieszczak.com.pl>
2023-01-04 21:13:52 +0100

    [System] Enable auto-optimise-store by default

--- nixos/playground/playground.nix
+++ nixos/playground/playground.nix
@@ -1,5 +1,22 @@
 { config, pkgs, lib, inputs, outputs, ... }:
 
 {
-
+  homelab = {
+    domain = "test.lan";
+    traefik = {
+      enable = true;
+      services.traefik = { port = 8080; authelia = true; };
+    };
+    multimedia = {
+      enable = true;
+      jellyfin.enable = true;
+      sonarr.enable = true;
+      radarr.enable = true;
+      prowlarr.enable = true;
+      bazarr.enable = true;
+      deluge = {
+        enable = true;
+      };
+    };
+  };
 }
