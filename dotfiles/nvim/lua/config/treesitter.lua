3f95276c4f939bf339bc3f9970140da4651e4277
Michał Mieszczak <michal@mieszczak.com.pl>
2023-01-19 10:17:46 +0100

    [Neovim] Use Oceanic colors in treesitter raibow

--- dotfiles/nvim/lua/config/treesitter.lua
+++ dotfiles/nvim/lua/config/treesitter.lua
@@ -15,7 +15,16 @@ require("nvim-treesitter.configs").setup({
 		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
 		max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
 		-- colors = {}, -- table of hex strings
-		-- termcolors = {} -- table of colour name strings
+		colors = {
+			"#EC5F67",
+			"#99C794",
+			"#FAC863",
+			"#6699CC",
+			"#C594C5",
+			"#5FB3B3",
+			"#D8DEE9",
+			"#f99157",
+		}
 	},
 	textobjects = {
 		select = {
