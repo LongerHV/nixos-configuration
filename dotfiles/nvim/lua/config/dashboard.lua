30b62967f2dc0f008c0166d8ae4f04da5a81c3fa
Michał Mieszczak <michal@mieszczak.com.pl>
2023-02-03 18:03:44 +0100

    [Neovim] Fix broken dashboard after upgrade

--- dotfiles/nvim/lua/config/dashboard.lua
+++ dotfiles/nvim/lua/config/dashboard.lua
@@ -1,8 +1,5 @@
-local db = require("dashboard")
-local remap = vim.api.nvim_set_keymap
-
--- Dashboard header
-db.custom_header = {
+local header = {
+	"",
 	"███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
 	"████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
 	"██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
@@ -10,30 +7,12 @@ db.custom_header = {
 	"██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
 	"╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
 	"",
-	"",
 }
 
--- Remaps
-remap("n", "<Leader>ss", "<cmd>SessionSave<CR>", {})
-remap("n", "<Leader>sl", "<cmd>SessionLoad<CR>", {})
-
-db.custom_center = {
-	{ icon = "  ",
-		desc = "Find  File                              ",
-		action = "Telescope find_files find_command=rg,--hidden,--files",
-		shortcut = "SPC f f" },
-	{ icon = "  ",
-		desc = "File Browser                            ",
-		action = "Telescope file_browser",
-		shortcut = "SPC f e" },
-	{ icon = "  ",
-		desc = "Live grep                               ",
-		action = "Telescope live_grep",
-		shortcut = "SPC f g" },
-}
-
--- vim.highlight.create("DashboardHeader", {guifg=vim.g.terminal_color_2})
--- vim.highlight.create("DashboardCenter", {guifg=vim.g.terminal_color_2})
--- vim.highlight.create("DashboardCenterIcon", {guifg=vim.g.terminal_color_4})
--- vim.highlight.create("DashboardShortCut", {guifg=vim.g.terminal_color_5})
--- vim.highlight.create("DashboardFooter", {guifg=vim.g.terminal_color_3})
+require("dashboard").setup({
+	theme = "hyper",
+	config = {
+		header = header,
+		packages = { enable = false },
+	}
+})
