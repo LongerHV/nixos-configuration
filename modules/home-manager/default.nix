c8bdeacc7129b5d2a081bbeba4c3dd152217d7a9
Michał Mieszczak <michal@mieszczak.com.pl>
2023-02-28 11:57:28 +0100

    [Home] Add default xdg configuration

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,9 +1,24 @@
 {
-  default = { lib, ... }: {
+  default = { config, lib, ... }: {
     home = rec {
       username = lib.mkDefault "longer";
       homeDirectory = lib.mkDefault "/home/${username}";
       stateVersion = lib.mkDefault "21.11";
+    };
+    xdg = {
+      enable = lib.mkDefault true;
+      userDirs = {
+        enable = lib.mkDefault true;
+        createDirectories = lib.mkDefault true;
+        desktop = lib.mkDefault "${config.home.homeDirectory}/Pulpit";
+        documents = lib.mkDefault "${config.home.homeDirectory}/Dokumenty";
+        download = lib.mkDefault "${config.home.homeDirectory}/Pobrane";
+        music = lib.mkDefault "${config.home.homeDirectory}/Muzyka";
+        pictures = lib.mkDefault "${config.home.homeDirectory}/Obrazy";
+        videos = lib.mkDefault "${config.home.homeDirectory}/Wideo";
+        templates = lib.mkDefault "${config.home.homeDirectory}/Szablony";
+        publicShare = lib.mkDefault "${config.home.homeDirectory}/Publiczny";
+      };
     };
   };
   myHome = import ./myHome;
