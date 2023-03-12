14f62a61ab343eea0d2f261da441edf88a807fd2
Michał Mieszczak <michal@mieszczak.com.pl>
2023-03-12 19:06:28 +0100

    [DolGuldur] Wait for proper interface to go online

--- nixos/dol-guldur/networking.nix
+++ nixos/dol-guldur/networking.nix
@@ -4,6 +4,9 @@
     enableIPv6 = false;
     dhcpcd.enable = false;
   };
-  systemd.network.enable = true;
+  systemd.network = {
+    enable = true;
+    wait-online.extraArgs = [ "--interface" "enp1s0" ];
+  };
   services.resolved.enable = true;
 }
