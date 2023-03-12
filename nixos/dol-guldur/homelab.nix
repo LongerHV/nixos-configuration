09a4cac2744c3e5c4328f5a579d03edb1db7d1a1
Michał Mieszczak <michal@mieszczak.com.pl>
2023-03-12 20:46:17 +0100

    [DolGuldur] Enable traefik, gitea and monitoring

--- nixos/dol-guldur/homelab.nix
+++ nixos/dol-guldur/homelab.nix
@@ -4,8 +4,10 @@
   homelab = {
     domain = "dol-guldur.longerhv.xyz";
     storage = "/data";
-    # traefik.enable = true;
-    # gitea.enable = true;
+    traefik.enable = true;
+    traefik.defaultIPWhitelist = "";
+    gitea.enable = true;
+    monitoring.enable = true;
     mysql = {
       enable = true;
       package = pkgs.mariadb_108;
