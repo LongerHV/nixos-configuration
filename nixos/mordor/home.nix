94f3513c82e6692c27536428a5aa80a397b7cd51
Michał Mieszczak <michal@mieszczak.com.pl>
2023-03-24 18:20:28 +0100

    [Mordor] Enable gsconnect

--- nixos/mordor/home.nix
+++ nixos/mordor/home.nix
@@ -22,10 +22,22 @@
     reaper
     signal-desktop
     super-slicer-latest
+
+    gnomeExtensions.tray-icons-reloaded
   ];
 
   services.nextcloud-client = {
     enable = true;
     startInBackground = true;
+  };
+
+  dconf.settings = {
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = [
+        "gsconnect@andyholmes.github.io"
+        "trayIconsReloaded@selfmade.pl"
+      ];
+    };
   };
 }
