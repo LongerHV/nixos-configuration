297b2d4ccffe1b0346d32942a8353c218f0a5fc5
Michał Mieszczak <michal@mieszczak.com.pl>
2023-03-28 10:12:45 +0200

    [DolGuldur] Remove unused traefik rule

--- nixos/dol-guldur/homelab.nix
+++ nixos/dol-guldur/homelab.nix
@@ -6,7 +6,6 @@
     storage = "/data";
     traefik.enable = true;
     traefik.defaultIPWhitelist = "";
-    traefik.services.lldap.port = 17170;
     gitea.enable = true;
     monitoring.enable = true;
     mysql = {
