bf146ca3b31127cce922e87296e71109ad3b49ec
Michał Mieszczak <michal@mieszczak.com.pl>
2023-05-13 17:23:34 +0200

    [HomeLab] Configure invidious, fix proxy setup

--- modules/nixos/homelab/invidious.nix
+++ modules/nixos/homelab/invidious.nix
@@ -2,6 +2,7 @@
 
 let
   cfg = config.homelab.invidious;
+  hasTLS = config.homelab.traefik.cloudflareTLS.enable;
 in
 {
   options.homelab.invidious = with lib; {
@@ -19,6 +20,19 @@ in
       enable = true;
       port = lib.mkDefault 3333;
       domain = lib.mkDefault "yt.${config.homelab.domain}";
+      settings = {
+        https_only = hasTLS;
+        external_port = if hasTLS then 443 else 80;
+        default_user_preferences = {
+          locale = "pl";
+          region = "PL";
+          captions = [ "Polish" "English" "Englich (auto-generated)" ];
+          feed_menu = [ "Subscriptions" "Playlists" ];
+          default_home = "Subscriptions";
+          player_style = "youtube";
+          quality = "dash";
+        };
+      };
     };
   };
 }
