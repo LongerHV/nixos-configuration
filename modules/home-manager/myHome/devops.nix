85a1b6be47ebe13f600c3e4e2ebb792c1272e856
Michał Mieszczak <michal@mieszczak.com.pl>
2023-07-04 11:25:48 +0200

    [DevOps] Refactor module

--- modules/home-manager/myHome/devops.nix
+++ modules/home-manager/myHome/devops.nix
@@ -2,29 +2,11 @@
 
 let
   cfg = config.myHome.devops;
-  kctx = with pkgs; (writeShellApplication {
-    name = "kctx";
-    runtimeInputs = [ kubectl fzf ];
-    text = ''
-      kubectl config get-contexts -o name \
-        | fzf --height=10 \
-        | xargs kubectl config use-context
-    '';
-  });
-  kctn = with pkgs; (writeShellApplication {
-    name = "kctn";
-    runtimeInputs = [ kubectl fzf ];
-    text = ''
-      kubectl get namespaces -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' \
-        | fzf --height=10 \
-        | xargs kubectl config set-context --current --namespace
-    '';
-  });
 in
 {
   options.myHome.devops.enable = lib.mkEnableOption "devops";
   config = lib.mkIf cfg.enable {
-    home.packages = (with pkgs; [
+    home.packages = with pkgs; [
       act
       ansible
       awscli2
@@ -46,9 +28,24 @@ in
       unstable.kubernetes-polaris
       unstable.kubeshark
       unstable.k9s
-    ]) ++ [
-      kctx
-      kctn
+      (writeShellApplication {
+        name = "kctx";
+        runtimeInputs = [ kubectl fzf ];
+        text = ''
+          kubectl config get-contexts -o name \
+          | fzf --height=10 \
+          | xargs kubectl config use-context
+        '';
+      })
+      (writeShellApplication {
+        name = "kctn";
+        runtimeInputs = [ kubectl fzf ];
+        text = ''
+          kubectl get namespaces -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' \
+            | fzf --height=10 \
+            | xargs kubectl config set-context --current --namespace
+        '';
+      })
     ];
   };
 }
