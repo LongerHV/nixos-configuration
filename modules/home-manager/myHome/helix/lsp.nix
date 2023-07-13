30ff4713e188666ecabd6fec7c9e1c3bda537044
Michał Mieszczak <michal@mieszczak.com.pl>
2023-07-13 18:07:29 +0200

    [Helix] Configure bash lsp

--- modules/home-manager/myHome/helix/lsp.nix
+++ modules/home-manager/myHome/helix/lsp.nix
@@ -40,5 +40,16 @@
       config.nil.formatting.command = [ "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt" ];
       auto-format = true;
     }
+    {
+      name = "bash";
+      scope = "source.bash";
+      injection-regex = "(shell|bash|zsh|sh)";
+      file-types = [ "sh" "bash" "zsh" ".bash_login" ".bash_logout" ".bash_profile" ".bashrc" ".profile" ".zshenv" ".zlogin" ".zlogout" ".zprofile" ".zshrc" "APKBUILD" "PKGBUILD" "eclass" "ebuild" "bazelrc" ".bash_aliases" ];
+      shebangs = [ "sh" "bash" "dash" "zsh" ];
+      roots = [ ];
+      comment-token = "#";
+      language-server = { command = "${pkgs.nodePackages.bash-language-server}/bin/bash-language-server"; args = [ "start" ]; };
+      indent = { tab-width = 2; unit = "  "; };
+    }
   ];
 }
