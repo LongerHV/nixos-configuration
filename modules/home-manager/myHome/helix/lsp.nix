274b0a76e941ba7543922256fd08404293e4b2e1
Michał Mieszczak <michal@mieszczak.com.pl>
2023-07-22 13:21:27 +0200

    [Helix] Configure markdown

--- modules/home-manager/myHome/helix/lsp.nix
+++ modules/home-manager/myHome/helix/lsp.nix
@@ -106,6 +106,23 @@ in
         command = "${taplo-cli}/bin/taplo";
         args = [ "lsp" "stdio" ];
       };
+      marksman = {
+        command = "${marksman}/bin/marksman";
+        args = [ "server" ];
+      };
+      efm-markdown = {
+        command = "${efm-langserver}/bin/efm-langserver";
+        config = {
+          documentFormatting = false;
+          languages.markdown = with nodePackages; [
+            {
+              lintCommand = "${markdownlint-cli}/bin/markdownlint --stdin";
+              lintStdin = true;
+              lintFormats = [ "%f:%l %m" "%f:%l:%c %m" "%f: %l: %m" ];
+            }
+          ];
+        };
+      };
     };
     language = [
       {
@@ -206,6 +223,23 @@ in
         comment-token = "#";
         language-servers = [ "taplo" ];
         indent = { tab-width = 2; unit = "  "; };
+      }
+      {
+        name = "markdown";
+        scope = "source.md";
+        injection-regex = "md|markdown";
+        file-types = [ "md" "markdown" "PULLREQ_EDITMSG" ];
+        roots = [ ".marksman.toml" ];
+        language-servers = [ "marksman" "efm-markdown" "efm-prettier" ];
+        indent = { tab-width = 2; unit = "  "; };
+      }
+      {
+        name = "markdown.inline";
+        scope = "source.markdown.inline";
+        injection-regex = "markdown\\.inline";
+        file-types = [ ];
+        roots = [ ];
+        grammar = "markdown_inline";
       }
     ];
   };
