0764e7b82d28688f13c8df30b25d6149f16d1170
Michał Mieszczak <michal@mieszczak.com.pl>
2023-09-04 08:31:31 +0200

    [Home] Configure gnome forge extension on Ubuntu

--- home-manager/work.nix
+++ home-manager/work.nix
@@ -22,4 +22,26 @@
 
   # Workaround for freezing during activity switching on Ubuntu
   dconf.settings."org/gnome/desktop/interface".enable-animations = false;
+
+  dconf.settings = {
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = [
+        "forge@jmmaranan.com"
+      ];
+    };
+    "org/gnome/shell/extensions/forge" = {
+      tiling-mode-enabled = true;
+      window-gap-size = 4;
+      window-gap-size-increment = 1;
+      window-gap-hidden-on-single = true;
+    };
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      screensaver = [ "<Shift><Control><Super>l" ];
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      minimize = [ "<Shift><Control><Super>h" ];
+      close = [ "<Super>x" ];
+    };
+  };
 }
