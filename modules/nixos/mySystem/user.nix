79530b071d37acdc62043ec54df4015d5dfabef2
Michał Mieszczak <michal@mieszczak.com.pl>
2023-11-22 17:49:16 +0100

    [Security] Move change ecdsa key for ed25519 for Fido2 auth

--- modules/nixos/mySystem/user.nix
+++ modules/nixos/mySystem/user.nix
@@ -33,8 +33,8 @@ in
         extraGroups = [ "wheel" "networkmanager" "keys" ];
         openssh.authorizedKeys.keys = [
           "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDCLO8jWFPCx141QQyBBSGFSEY1iGwrcrb0NnNfjDHopx+FDPSo3d8Rat9sMqojL9o80frLxU/SpkC/9BddCu7dqlmPFEt2rNvzG2Evv+Epovr/hHD5EeJP7fNdW+FqoODIK9GOJLstc5h8m7LdMwEpI7FlSVRbhBFhiwwhdbIlGNnFogDggjc9WIux5oyzY6i3O/GNeP/G9Mwi8STGGKS0yuBVtVmsJ+zakrXWpSAhm4N0OSZzxUKGAzLWCs67VnF4VM+/nhCqro9jlpORDyM19AmMtAC/M2NI8T/Um0UaUm/I3wFkOCRqRdbNk6M6pCmTGm6jOszugNjb8zUH4lT1KfSZbo/GIO0Lyxi3bPCQFQLl0r6aVMn0AIOkkNmPg4LvVa7ux9FlaE1qjEoe6TtkZ2i50+4FWWS2ZcFJpiNDQ8crc4TNnrjeOkye4E//gw+6ki3UaTETR7ZwsnnjiTLFw2aJcP8BGOZBVvjmkKSIZ6cLhyo0rc+yamxcWaCup27g8xxLlD6CXDwmvRz04KyxUJf6eBGmX58d3m2zhbDC9pJXh0I5HtbOydTLgY7wDFnLx9p6yNPSLyD4jotKJdCH5IjFL1s41/YzpunkhNRyWvNCLUBS5xiE+4BmFcTFWsov1dwd3+uriR5/Q7iMCnvl2kadAkRcjCcLR3vJKNwfqQ== longer@mordor"
-          "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBPk7RSeptUUytK4whG+3YVOgjCvOWMW5CctSKXeNOjkcfmKLZ6fTn32ymvtLpeSa9vGSTnhy6QyitlJEyKHiEOwAAAAIc3NoOnl1Ymk= ssh:yubi"
-          "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBKHyzabGGBrRpZT/h/yJPMiSh7UGsrH/cmT9bkMIjqBTN6D9otYj1KXLoukUQVK2ZyHTrzvVc2fV1tGOKGBUxegAAAAPc3NoOnl1YmktYmFja3Vw ssh:yubi-backup"
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJeDAPNMkPJhO1E8Dli8G7ymP4SQKrLcT54MZYJeWURoAAAACHNzaDp5dWJp ssh:yubi"
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHxKfDtb9EWDrIVAAx289XxLVtdVWpZLoisvK2OEENAuAAAAD3NzaDp5dWJpX2JhY2t1cA== ssh:yubi_backup"
         ];
       };
     };
