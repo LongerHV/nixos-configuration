e8735e5704295dc3d487ee260ab86e8ed0b49f6b
Michał Mieszczak <michal@mieszczak.com.pl>
2024-02-04 13:18:21 +0100

    [Neovim] Fix plugins module

--- modules/home-manager/myHome/neovim/plugins/default.nix
+++ modules/home-manager/myHome/neovim/plugins/default.nix
@@ -2,41 +2,43 @@
 
 let
   cfg = config.myHome.neovim;
-  importPlugins = plugins: map (path: import path args) plugins;
+  importPlugins = plugins: (map (path: import path args) plugins);
+  plugins = (importPlugins [
+    ./telescope.nix
+    ./treesitter.nix
+  ]) ++ [
+    {
+      plugin = pkgs.nvimPlugins.mini;
+      configFile = ./mini.lua;
+    }
+    {
+      plugin = pkgs.nvimPlugins.indent-blankline;
+      name = "ibl";
+      opts.scope.enabled = false;
+    }
+    { plugin = pkgs.nvimPlugins.oceanic-next; }
+    { plugin = pkgs.nvimPlugins.devicons; }
+    { plugin = pkgs.nvimPlugins.gitsigns; opts = { }; }
+    { plugin = pkgs.nvimPlugins.dressing; opts = { }; }
+    { plugin = pkgs.nvimPlugins.gen; }
+    {
+      plugin = pkgs.nvimPlugins.oil;
+      opts.view_options.show_hidden = true;
+      postConfig = ''
+        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+      '';
+    }
+    { plugin = pkgs.vimPlugins.vim-sleuth; }
+  ];
+  lspPlugins = (importPlugins [
+    ./lspconfig.nix
+    ./cmp.nix
+  ]) ++ [
+    { plugin = pkgs.nvimPlugins.fidget; opts = { }; }
+  ];
 in
 {
-  config = lib.mkIf cfg.enable
-    {
-      myHome.neovim.plugins = (importPlugins [
-        ./telescope.nix
-        ./treesitter.nix
-      ]) ++ [
-        {
-          plugin = pkgs.nvimPlugins.mini;
-          configFile = ./mini.lua;
-        }
-        {
-          plugin = pkgs.nvimPlugins.indent-blankline;
-          name = "ibl";
-          opts.scope.enabled = false;
-        }
-        { plugin = pkgs.nvimPlugins.oceanic-next; }
-        { plugin = pkgs.nvimPlugins.devicons; }
-        { plugin = pkgs.nvimPlugins.gitsigns; opts = { }; }
-        { plugin = pkgs.nvimPlugins.dressing; opts = { }; }
-        { plugin = pkgs.nvimPlugins.gen; }
-        {
-          plugin = pkgs.nvimPlugins.oil;
-          opts.view_options.show_hidden = true;
-          postConfig = ''
-            vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-          '';
-        }
-        { plugin = pkgs.vimPlugins.vim-sleuth; }
-      ] ++ (lib.attrsets.optionalAttrs cfg.enableLSP (importPlugins [
-        { plugin = pkgs.nvimPlugins.fidget; opts = { }; }
-        ./lspconfig.nix
-        ./cmp.nix
-      ]));
-    };
+  config = lib.mkIf cfg.enable {
+    myHome.neovim.plugins = plugins ++ (lib.attrsets.optionalAttrs cfg.enableLSP lspPlugins);
+  };
 }
