6634187f0c5f5464f348055c0c04e604a2be8b90
Michał Mieszczak <michal@mieszczak.com.pl>
2025-02-10 10:52:36 +0100

    [DevOps] Update kubectl to 1.29.x

--- home-manager/work.nix
+++ home-manager/work.nix
@@ -19,7 +19,7 @@
   };
 
   nixpkgs.overlays = [
-    (final: prev: { kubectl = prev.kubectl_1_28; })
+    (final: prev: { kubectl = prev.kubectl_1_29; })
   ];
 
   # Workaround for freezing during activity switching on Ubuntu
