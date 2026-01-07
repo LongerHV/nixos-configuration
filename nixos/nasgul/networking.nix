fd8dcfd0ced7eb5476af32f847f6985db572adcc
Michał Mieszczak <michal@mieszczak.com.pl>
2026-01-07 21:28:50 +0100

    [Nasgul] Migrate to systemd-networkd and nftables

--- nixos/nasgul/networking.nix
+++ nixos/nasgul/networking.nix
@@ -2,14 +2,26 @@
   networking = {
     hostName = "nasgul";
     hostId = "48392063";
-    useDHCP = false;
     enableIPv6 = false;
     usePredictableInterfaceNames = false;
-    interfaces.eth0.useDHCP = true;
+    useNetworkd = true;
+    dhcpcd.enable = false;
+    nftables.enable = true;
     nat = {
       enable = true;
       externalInterface = "eth0";
     };
     iproute2.enable = true;
   };
+  systemd.network = {
+    enable = true;
+    networks."10-eth0" = {
+      matchConfig.Name = "eth0";
+      networkConfig = {
+        DHCP = "yes";
+      };
+      dhcpV4Config.UseDNS = false; # Conflicts with Blocky DNS
+    };
+  };
+  services.resolved.enable = false; # Conflicts with Blocky DNS
 }
