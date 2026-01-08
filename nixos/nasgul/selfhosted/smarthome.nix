4145da2d66f41411a276cc05fb363e0823075da1
Michał Mieszczak <michal@mieszczak.com.pl>
2026-01-09 00:37:47 +0100

    [Flake] Fix statix warning

--- nixos/nasgul/selfhosted/smarthome.nix
+++ nixos/nasgul/selfhosted/smarthome.nix
@@ -5,79 +5,82 @@
   # not sure how crutial it is for operation
   # networking.firewall.trustedInterfaces = [ "vlan20" ];
 
-  networking.firewall.allowedUDPPorts = [ 5353 ]; # mDNS
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    publish = {
+  networking.firewall.allowedUDPPorts = [ 5353 ];
+  services = {
+    # mDNS
+    avahi = {
       enable = true;
-      addresses = true;
-      domain = true;
-      hinfo = true;
-      userServices = true;
-      workstation = true;
+      nssmdns4 = true;
+      publish = {
+        enable = true;
+        addresses = true;
+        domain = true;
+        hinfo = true;
+        userServices = true;
+        workstation = true;
+      };
+      allowInterfaces = [ "eth0" "vlan20" ];
     };
-    allowInterfaces = [ "eth0" "vlan20" ];
-  };
 
-  services.matter-server = {
-    enable = true;
-    # extraArgs = [ "--primary-interface" "vlan20" ];
-  };
-  services.home-assistant = {
-    enable = true;
-    extraPackages = ps: with ps; [ psycopg2 ];
-    extraComponents = [
-      # Components required to complete the onboarding
-      "analytics"
-      "google_translate"
-      "met"
-      "radio_browser"
-      "shopping_list"
-      # Recommended for fast zlib compression
-      "isal"
+    matter-server = {
+      enable = true;
+      # extraArgs = [ "--primary-interface" "vlan20" ];
+    };
+    home-assistant = {
+      enable = true;
+      extraPackages = ps: with ps; [ psycopg2 ];
+      extraComponents = [
+        # Components required to complete the onboarding
+        "analytics"
+        "google_translate"
+        "met"
+        "radio_browser"
+        "shopping_list"
+        # Recommended for fast zlib compression
+        "isal"
 
-      "matter"
-    ];
-    customComponents = with pkgs.home-assistant-custom-components; [
-      auth_oidc
-    ];
-    config = {
-      default_config = { };
-      homeassistant = {
-        internal_url = "https://hass.${config.homelab.domain}";
+        "matter"
+      ];
+      customComponents = with pkgs.home-assistant-custom-components; [
+        auth_oidc
+      ];
+      config = {
+        default_config = { };
+        homeassistant = {
+          internal_url = "https://hass.${config.homelab.domain}";
+        };
+        discovery = {
+          ignore = [
+            { platform = "cast"; }
+            { platform = "ipp"; }
+            { platform = "androidtv_remote"; }
+          ];
+        };
+        http = {
+          server_host = [ "127.0.0.1" ];
+          trusted_proxies = [ "127.0.0.1" ];
+          use_x_forwarded_for = true;
+        };
+        recorder.db_url = "postgresql://@/hass";
+        "automation ui" = "!include automations.yaml";
+        auth_oidc = {
+          client_id = "hass";
+          client_secret = "!env_var HASS_OIDC_SECRET";
+          discovery_url = "https://auth.${config.homelab.domain}/.well-known/openid-configuration";
+          # features.automatic_user_linking = true; # Use only for initial setup to link first (admin) user created by HASS
+        };
       };
-      discovery = {
-        ignore = [
-          { platform = "cast"; }
-          { platform = "ipp"; }
-          { platform = "androidtv_remote"; }
-        ];
-      };
-      http = {
-        server_host = [ "127.0.0.1" ];
-        trusted_proxies = [ "127.0.0.1" ];
-        use_x_forwarded_for = true;
-      };
-      recorder.db_url = "postgresql://@/hass";
-      "automation ui" = "!include automations.yaml";
-      auth_oidc = {
-        client_id = "hass";
-        client_secret = "!env_var HASS_OIDC_SECRET";
-        discovery_url = "https://auth.${config.homelab.domain}/.well-known/openid-configuration";
-        # features.automatic_user_linking = true; # Use only for initial setup to link first (admin) user created by HASS
-      };
+    };
+    postgresql = {
+      ensureDatabases = [ "hass" ];
+      ensureUsers = [{
+        name = "hass";
+        ensureDBOwnership = true;
+      }];
     };
   };
   age.secrets.hass_environment.file = ../../../secrets/nasgul_hass_environment.age;
   systemd.services.home-assistant.serviceConfig.EnvironmentFile = config.age.secrets.hass_environment.path;
-  services.postgresql = {
-    ensureDatabases = [ "hass" ];
-    ensureUsers = [{
-      name = "hass";
-      ensureDBOwnership = true;
-    }];
-  };
   systemd.tmpfiles.rules = [
     "f ${config.services.home-assistant.configDir}/automations.yaml 0644 hass hass"
   ];
