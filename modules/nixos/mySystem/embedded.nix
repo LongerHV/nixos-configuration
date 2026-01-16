9fcd10a5f501b669bfd29feef35633b2444c5863
Michał Mieszczak <michal@mieszczak.com.pl>
2026-01-16 22:00:47 +0100

    [Embedded] Fix openocd

--- modules/nixos/mySystem/embedded.nix
+++ modules/nixos/mySystem/embedded.nix
@@ -9,6 +9,8 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    users.groups.plugdev = { };
+    users.users."${config.mySystem.user}".extraGroups = [ "plugdev" ];
     programs.nix-ld.enable = true;
     services.udev.packages = with pkgs; [ openocd ];
   };
