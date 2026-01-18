940a01c4ad9da6157b1f4a9283777a58ccd6e79b
Michał Mieszczak <michal@mieszczak.com.pl>
2026-01-18 10:11:51 +0100

    [Nasgul] Reconfigure docker to use nftables

--- nixos/nasgul/selfhosted/containers.nix
+++ nixos/nasgul/selfhosted/containers.nix
@@ -1,7 +1,14 @@
+{ pkgs, ... }:
+
 {
   virtualisation.oci-containers.backend = "docker";
   virtualisation.docker = {
     enable = true;
     storageDriver = "zfs";
+
+    # Use Docker 29 for compatibility with  nftables
+    package = pkgs.docker_29;
+    extraOptions = "--firewall-backend=nftables";
+    extraPackages = [ pkgs.nftables ];
   };
 }
