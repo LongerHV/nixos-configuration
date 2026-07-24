0c1f3e312ab64a1a1826c3a51a639021a4c2196e
Michał Mieszczak <michal@mieszczak.com.pl>
2026-07-24 13:23:23 +0200

    [Smaug] Use minimal cli setup

--- nixos/smaug/home.nix
+++ nixos/smaug/home.nix
@@ -1,5 +1,8 @@
 {
   home.stateVersion = "22.11";
-  myHome.zsh.enable = true;
-  myHome.neovim.enable = true;
+  myHome = {
+    zsh.enable = true;
+    cli.minimal = true;
+    neovim.enable = true;
+  };
 }
